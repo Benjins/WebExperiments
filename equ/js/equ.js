4dc51c2a776c66a91d67a7a217e561602b8d20a2
Benji Smith <benjsith@gmail.com>
2018-01-13 12:41:39 -0800

    equ stuff

--- equ/js/equ.js
+++ equ/js/equ.js
@@ -3,16 +3,161 @@ var OPS = {
 	PUSH_LIT: 1,
 	ADD: 2,
 	SUB: 3,
-	MULTIPLY: 4,
-	DIVIDE: 5
+	MUL: 4,
+	DIV: 5
 };
 
 var cachedParseRPN = [];
 
+var digits = '0123456789';
+var punctuation = '{}+-*/()';
+
 function parseEquation() {
 	var code = document.getElementById('equ').value;
 	
-	cachedParseRPN = [OPS.PUSH_ARG, 0, OPS.PUSH_LIT, 5, OPS.MULTIPLY, OPS.PUSH_LIT, 2, OPS.ADD];
+	var tokens = [];
+	for (var i = 0; i < code.length; i++) {
+		var c = code.charAt(i);
+		if (punctuation.indexOf(c) !== -1) {
+			tokens.push(c);
+		} else if (digits.indexOf(c) !== -1) {
+			var val = 0;
+			for (; i < code.length; i++) {
+				c = code.charAt(i);
+				var digit = digits.indexOf(c);
+				if (digit === -1) {
+					i--;
+					break;
+				} else {
+					val = val * 10 + digit;
+				}
+			}
+			
+			tokens.push(val);
+		}
+	}
+	
+	console.log(tokens);
+	
+	cachedParseRPN = [];
+	
+// while there are tokens to be read:
+	// read a token.
+	// if the token is a number, then push it to the output queue.
+	// if the token is an operator, then:
+		// while ((there is an operator at the top of the operator stack with
+			// greater precedence) or (the operator at the top of the operator stack has
+                        // equal precedence and
+                        // the operator is left associative)) and
+                      // (the operator at the top of the stack is not a left bracket):
+				// pop operators from the operator stack, onto the output queue.
+		// push the read operator onto the operator stack.
+	// if the token is a left bracket (i.e. "("), then:
+		// push it onto the operator stack.
+	// if the token is a right bracket (i.e. ")"), then:
+		// while the operator at the top of the operator stack is not a left bracket:
+			// pop operators from the operator stack onto the output queue.
+		// pop the left bracket from the stack.
+		// /* if the stack runs out without finding a left bracket, then there are
+		// mismatched parentheses. */
+// if there are no more tokens to read:
+	// while there are still operator tokens on the stack:
+		// /* if the operator token on the top of the stack is a bracket, then
+		// there are mismatched parentheses. */
+		// pop the operator onto the output queue.
+// exit.
+	
+	{
+		var tokToOpMap = {};
+		tokToOpMap['+'] = OPS.ADD;
+		tokToOpMap['-'] = OPS.SUB;
+		tokToOpMap['*'] = OPS.MUL;
+		tokToOpMap['/'] = OPS.DIV;
+		var opToPrecMap = {};
+		opToPrecMap[OPS.ADD] = 3;
+		opToPrecMap[OPS.SUB] = 3;
+		opToPrecMap[OPS.MUL] = 4;
+		opToPrecMap[OPS.DIV] = 4;
+		
+		var opStack = [];
+		for (var i = 0; i < tokens.length; i++) {
+			var tok = tokens[i];
+			if (typeof(tok) === 'number') {
+				cachedParseRPN.push(OPS.PUSH_LIT);
+				cachedParseRPN.push(tok);
+			} else if (tok === '{') {
+				if (i < tokens.length - 2) {
+					if (typeof(tokens[i+1]) === 'number') {
+						if (tokens[i+2] === '}') {
+							cachedParseRPN.push(OPS.PUSH_ARG);
+							cachedParseRPN.push(tokens[i+1]);
+							i += 2;
+						} else {
+							console.log("BAD ARG IDX");
+						}
+					} else {
+						console.log("BAD ARG IDX");
+					}
+				} else {
+					console.log("BAD ARG IDX");
+				}
+			} else if (tok === '(') {
+				opStack.push(tok);
+			} else if (tok === ')') {
+				while(opStack.length > 0) {
+					if (opStack[opStack.length - 1] === '(') {
+						opStack.pop();
+						break;
+					} else {
+						cachedParseRPN.push(opStack.pop());
+					}
+				}
+			} else {
+				var op = tokToOpMap[tok];
+				if (op === undefined) {
+					console.log("BAD TOKEN: " + tok);
+				} else {
+					while (opStack.length > 0) {
+						var topOp = opStack[opStack.length - 1];
+						if (topOp === '(') {
+							break;
+						} else {
+							if (opToPrecMap[topOp] >= opToPrecMap[op]) {
+								cachedParseRPN.push(topOp);
+							} else {
+								break;
+							}
+						}
+						
+						opStack.pop();
+					}
+					
+					opStack.push(op);
+				}
+			}
+			
+			console.log("opStack: " + JSON.stringify(opStack));
+		}
+		
+		while (opStack.length > 0) {
+			var topOp = opStack.pop();
+			if (topOp === '(' || topOp === ')') {
+				break;
+			} else {
+				cachedParseRPN.push(topOp);
+			}
+		}
+	}
+	
+	for (var i = 0; i < cachedParseRPN.length; i++) {
+		if (typeof(cachedParseRPN[i]) !== 'number') {
+			console.log("BAD RPN OP: rpn[" + i + "] = " + cachedParseRPN[i]);
+		}
+	}
+	
+	console.log(cachedParseRPN);
+	
+	//cachedParseRPN = [OPS.PUSH_ARG, 0, OPS.PUSH_LIT, 5, OPS.MUL, OPS.PUSH_LIT, 2, OPS.ADD];
 }
 
 function updateDisplay() {
@@ -26,7 +171,7 @@ function updateDisplay() {
 	for (var idx = 0; idx < cachedParseRPN.length; idx++) {
 		if (cachedParseRPN[idx] == OPS.PUSH_ARG) {
 			if (idx < cachedParseRPN.length - 1) {
-				stack.push(slid.value);
+				stack.push(parseInt(slid.value));
 				idx++;
 			} else {
 				console.log("BAD PUSH_ARG");
@@ -42,7 +187,7 @@ function updateDisplay() {
 			if (stack.length >= 2) {
 				var a = stack.pop();
 				var b = stack.pop();
-				stack.push(a + b);
+				stack.push(b + a);
 			} else {
 				console.log("BAD ADD");
 			}
@@ -50,31 +195,31 @@ function updateDisplay() {
 			if (stack.length >= 2) {
 				var a = stack.pop();
 				var b = stack.pop();
-				stack.push(a - b);
+				stack.push(b - a);
 			} else {
 				console.log("BAD ADD");
 			}
-		} else if (cachedParseRPN[idx] == OPS.MULTIPLY) {
+		} else if (cachedParseRPN[idx] == OPS.MUL) {
 			if (stack.length >= 2) {
 				var a = stack.pop();
 				var b = stack.pop();
-				stack.push(a * b);
+				stack.push(b * a);
 			} else {
-				console.log("BAD MULTIPLY");
+				console.log("BAD MUL");
 			}
-		} else if (cachedParseRPN[idx] == OPS.DIVIDE) {
+		} else if (cachedParseRPN[idx] == OPS.DIV) {
 			if (stack.length >= 2) {
 				var a = stack.pop();
 				var b = stack.pop();
-				stack.push(a / b);
+				stack.push(b / a);
 			} else {
-				console.log("BAD DIVIDE");
+				console.log("BAD DIV");
 			}
 		} else {
 			console.log("Invalid op: " + cachedParseRPN[idx]);
 		}
 		
-		console.log("stack: " + stack);
+		//console.log("stack: " + JSON.stringify(stack));
 	}
 	
 	if (stack.length != 1) {
@@ -89,6 +234,7 @@ window.onload = function(){
 	updateDisplay();
 
 	document.getElementById("slid11").oninput = updateDisplay;
+	document.getElementById("equ").onchange = function() { parseEquation(); updateDisplay(); }
 };
 
 
